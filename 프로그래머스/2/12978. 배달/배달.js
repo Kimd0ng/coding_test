4c45ffbb8e451891121b3e5af0b039046ca94bd2
Kimd0ng <82995838+Kimd0ng@users.noreply.github.com>
2025-11-15 19:08:16 +0900

    [level 2] Title: 배달, Time: 16.23 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,5 +1,62 @@
 function solution(N, road, K) {
     
+    class PriorityQueue {
+        constructor() {
+            this.heap = [];
+        }
+        
+        size() {
+            return this.heap.lenght;
+        }
+        
+        isEmpty() {
+            return this.heap.length === 0;
+        }
+        
+        swap(idx1, idx2) {
+            [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]]
+        }
+        
+        enqueue(to, dist) {
+            this.heap.push({to, dist});
+            this.heapifyUp(this.heap.length - 1);
+        }
+        
+        heapifyUp(index) {
+            while (index > 0) {
+                const parentIndex = (index - 1) >> 1;
+                if (this.heap[parentIndex].dist <= this.heap[index].dist) break;
+                this.swap(parentIndex, index);
+                index = parentIndex;
+            }
+        }
+        
+        denqueue() {
+            const min = this.heap[0];
+            const end = this.heap.pop();
+            if (this.heap.length > 0) {
+                this.heap[0] = end;
+                this.heapifyDown(0);
+            }
+            return min;
+        }
+        
+        heapifyDown(index) {
+            while (index < this.heap.length) {
+                const left = (index << 1) + 1;
+                const right = (index << 1) + 2;
+                let smallest = index;
+                if (this.heap[left] && this.heap[left].dist < this.heap[smallest].dist)
+                    smallest = left;
+                if (this.heap[right] && this.heap[right].dist < this.heap[smallest].dist)
+                    smallest = right;
+                if (smallest === index) break;
+                this.swap(index, smallest);
+                index = smallest;
+            }
+        }
+    }
+    
     var answer = 0;
     
     const graph = Array.from({length: N+1}, () => Array().fill([]));
@@ -14,28 +71,49 @@ function solution(N, road, K) {
     //     graph[road[i][1]].push({to: road[i][0], dist:road[i][2]});
     // }
     
-    const dist = Array(N + 1).fill(Infinity);
+    const distance = Array(N + 1).fill(Infinity);
     
-    const queue = [{to:1, dist:0}];
-    
-    dist[1] = 0;
-    
-    while (queue.length > 0) {
-        const {to} = queue.pop();
-        
-        graph[to].forEach((Next) => {
-            const acc = dist[to] + Next.dist;
-            if (dist[Next.to] > acc) {
-                dist[Next.to] = acc;
-                queue.push(Next);
+    function dijkstra(start) {
+        const pq = new PriorityQueue();
+        pq.enqueue(start, 0);
+        distance[start] = 0;
+        while (!pq.isEmpty()) {
+            let {to, dist} = pq.denqueue();
+            if (distance[to] < dist) continue;
+            for (let Next of graph[to]){
+                const acc = distance[to] + Next.dist;
+                if (distance[Next.to] > acc) {
+                    distance[Next.to] = acc;
+                    pq.enqueue(Next.to, acc);
+                }
             }
-        });
+        }
     }
     
-    console.log(dist);
+    dijkstra(1);
     
-    for (let i = 0; i < dist.length; i++)
-        if (dist[i] <= K) answer++;
+    console.log(distance);
+    
+    for (let i = 0; i < distance.length; i++)
+        if (distance[i] <= K) answer++;
+    
+    
+//     const queue = [{to:1, dist:0}];
+    
+//     dist[1] = 0;
+    
+//     while (queue.length > 0) {
+//         const {to} = queue.pop();
+        
+//         graph[to].forEach((Next) => {
+//             const acc = dist[to] + Next.dist;
+//             if (dist[Next.to] > acc) {
+//                 dist[Next.to] = acc;
+//                 queue.push(Next);
+//             }
+//         });
+//     }
+    
     
     return answer;
 }
