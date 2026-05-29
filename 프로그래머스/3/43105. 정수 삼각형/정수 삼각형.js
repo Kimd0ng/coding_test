02b8f13e461fc52118be326bae8c50a6abaa01d8
Kimd0ng <82995838+Kimd0ng@users.noreply.github.com>
2026-05-29 20:37:23 +0900

    [level 3] Title: 정수 삼각형, Time: 3.54 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,12 +1,26 @@
 function solution(triangle) {
-    //삼각형의 하단부에서 부터 위로 올라가도록 구현
-    const dp = triangle[triangle.length-1].slice()
+    var answer = 0;
     
-    //가장 하단부와 바로 윗줄을 확인하여 윗줄에서 하단부의 2가지중 최대값을 선택하여 합하도록 지정
-    for(let i = triangle.length - 2; i >= 0; i--){
-        for (let j = 0; j <= i; j++)
-            dp[j] = triangle[i][j] + Math.max(dp[j], dp[j+1]);
+    // 아래서부터 위로 올라올거임
+    // 4번째 줄의 입장에서 2가지 경로중 당길수 있는 큰수를 당깅거임
+        // 7 12 10 10
+    // 3번째 줄의 입장에서
+        // 20 13 10
+    // 2번째 줄의 입장
+        // 23 21
+    // 1번째 줄의 입장
+        // 30
+    // 2가지 중에 큰걸 선택하면 무조건 합이 큰값에 결론 도달
+    
+    let num = triangle.length;
+    
+    for (let i = 0; i < num - 1; i++) {
+        for (let j = 0; j < num - i - 1; j++) {
+            triangle[num - i - 2][j] += Math.max(triangle[num - i - 1][j], triangle[num - i - 1][j + 1]);
+        }
     }
     
-    return dp[0];
+
+    
+    return triangle[0][0];
 }
